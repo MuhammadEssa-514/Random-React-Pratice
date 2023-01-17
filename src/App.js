82a7b737e91137a597a03fe3c3e5b1f6242886eb
MuhammadEssa-514 <muhammadessa1512141@gmail.com>
2023-01-17 18:37:31 +0500

    heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
    <>
+   <h1>hellow salam muhammad sultani</h1>
+   <p>hey i a muhammad essa hope you all are fine i beg to say that i cannot came to school due to some important work</p>
    <Navbar/>
    <Home/>
    </>
