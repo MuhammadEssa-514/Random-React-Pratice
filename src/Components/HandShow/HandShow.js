1ac50801249432f75042a3847527ed94bbc10a19
MuhammadEssa-514 <muhammadessa1512141@gmail.com>
2023-08-07 13:08:32 +0500

    fe

--- src/Components/HandShow/HandShow.js
+++ src/Components/HandShow/HandShow.js
@@ -25,6 +25,9 @@ function HandShow() {
   };
  const [count,setCount]=useState(10);
  const [text,setText]=useState("MUhammadhmas ");
+ const [name,setName]=useState("Ahmad");
+ const [fname,setFname]=useState("Julam");
+ const [clas,setClas]=useState(9);
 
   return (
     <>
