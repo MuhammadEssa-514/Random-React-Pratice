e7c78f3b855796aea73bf397a2d17ff9f3d58e7c
MuhammadEssa-514 <muhammadessa1512141@gmail.com>
2023-08-07 13:34:36 +0500

    iuf

--- src/Components/Hooks/UseState.js
+++ src/Components/Hooks/UseState.js
@@ -5,10 +5,29 @@ function UseState() {
      {/* for input start */}
     const [count,setCount]=useState(10);
     const [text,setText]=useState("MUhammadhmas ");
-     {/* for input end */}
+     {/* for input end=========== */}
+
+     {/* for heading student detail start */}
     const [name,setName]=useState("Ahmad");
     const [fname,setFname]=useState("Julam");
     const [clas,setClas]=useState(9);
+
+    const data=()=>{
+        setName("MuhammadEssa-514");
+        setFname("Musa Ali");
+        setClas(14);
+    }
+     {/* for heading student detail end====== */}
+
+     {/* for shirt Start */}
+    const [shirt,setShirt]=useState({
+        color:"green",
+        size:l,
+        price:1400,
+    })
+     {/* for shirt End====== */}
+
+
 
   return (
     <>
@@ -25,7 +44,19 @@ function UseState() {
       />
       {text}
     </div>
-     {/* for input end */}
+     {/* for input end=============== */}
+{/* ======================================================================================= */}
+     {/* for heading student detail start */}
+      <h1>student detail</h1>
+      <p>my name is ,{name}, and my father name is ,{fname}, and i am in {clas} class</p>
+      <button onClick={data} className='bg-black text-white px-3 py-2 rounded'>change student data in above line</button>
+     {/* for heading student detail end========= */}
+{/* ======================================================================================= */}
+     {/* for shirt Start */}
+     <p>shirt color is {shirt.color} and size is {shirt.size} and final price is {shirt.price}</p>
+     {/* for shirt end */}
+
+
 
     </>
   )
